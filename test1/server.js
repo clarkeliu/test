dfc522819fdc84f0f23efb7206a3be1fb3d9025a
clarkeliu <lk19831124635@msn.com>
2016-08-25 09:45:21 +0800

    first change in master

--- test1/server.js
+++ test1/server.js
@@ -4,6 +4,7 @@ var url = require("url");
 function start(routeproc) {
 
 	function execute(request, response) {
+		console.log("request received.");
 		var pathname = url.parse(request.url).pathname;
 		var query = url.parse(request.url).query;
 		routeproc(pathname, query, response);
